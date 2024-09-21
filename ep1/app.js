2c77b0d4f06ba81e4b868fc21ec1a819431c1bb5
kartikey sharma <kartikey7518@gmail.com>
2024-09-22 02:55:37 +0530

    multi children in createElement

--- ep1/app.js
+++ ep1/app.js
@@ -1,10 +1,36 @@
-const h1 = React.createElement(
+/*
+const parent = React.createElement("div", {id : "parent"}, React.createElement("div", {id: "child"}, 
+    React.createElement("h1", {}, "I'm h1 tag")
+)); //-----> in case of single children
+*/
+
+/*
+const parent = React.createElement("div", {id : "parent"}, React.createElement("div", {id: "child"}, 
+    [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]
+));  //----> for multiple child we can put all of them in an array
+*/
+
+
+const parent = React.createElement("div", {id : "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+    React.createElement("h1", {}, "I'm h1 tag"), 
+    React.createElement("h2", {}, "I'm h2 tag")]
+),
+    React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "I'm h1 tag"), 
+    React.createElement("h2", {}, "I'm h2 tag")]
+)]
+);
+
+
+const h1 = React.createElement( //createElement is object
     "h1", 
     {id : "heading"},
     "Hello world from react"
 );
 
-console.log(h1) //this will return obj to you
+console.log(parent) //this will return obj to you
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(h1); //take the createElement obj then create that tag which browser understand and then render it to root
+
+root.render(parent); // * take the createElement obj then create that tag which browser understand and then render it to root.  * replaces the content inside the html root not append  * this will only render inside the "root" div not the entire html
